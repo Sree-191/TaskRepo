f23184f2dc8d1e6548819ea73df5ed659300bd84
GIT2026 <sreenandinikolapalli@gmail.com>
2026-07-06 15:35:17 +0100

    Modified the Demo File

--- Demo1.py
+++ Demo1.py
@@ -1,27 +1,2 @@
-tasks = []
-
-while True:
-    print("\n===== TO-DO APP =====")
-    print("1. Add Task")
-    print("2. View Tasks")
-    print("3. Exit")
-
-    choice = input("Enter your choice: ")
-
-    if choice == "1":
-        task = input("Enter task: ")
-        tasks.append(task)
-        print("Task added!")
-
-    elif choice == "2":
-        print("\nTasks:")
-        for task in tasks:
-            print("-", task)
-
-    elif choice == "3":
-        break
-
-    else:
-        print("Invalid choice")
-    print("Git is awsome!")
-    print("This line is NOT staged")
+print("Git is awsome!")
+print("This line is NOT staged")
